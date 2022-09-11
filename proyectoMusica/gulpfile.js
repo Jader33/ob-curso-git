5181ea2a898dc1b404ecbc03bd8200b009c0e8cf
Jader33 <piedra1966@hotmail.com>
2022-09-11 12:17:22 -0500

    original

--- proyectoMusica/gulpfile.js
+++ proyectoMusica/gulpfile.js
@@ -1,17 +1,20 @@
 const {src,dest,watch} = require("gulp")
-const sass = require("gulp-sass")(require("sass"))
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(params) {
     //identificar el archivo sass
 //compilar
 
-src("src/scss/app.scss").pipe(sass())
+src("src/scss/**/*.scss")
+.pipe(plumber())
+.pipe(sass())
 .pipe(dest("build/css"))
 
     params()//colba que avisa agulp cuando llego al  final
 }
 function dev(done){
-    watch("src/scss/app.scss",css)
+    watch("src/scss/**/*.scss",css)
 
     done()
 }
